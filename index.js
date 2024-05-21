a625e99d1abd09da185e2af090a131b2d7d62b7a
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-21 10:23:35 -0300

    Creacion api

--- index.js
+++ index.js
@@ -1,24 +1,86 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require("dotenv").config();
 
-// Basic Configuration
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { isValidUrl } = require("./src/utils");
+
+const { Shortener } = require("./src/mongoose");
+
+// -------------
+//  Middlewares
+// -------------
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// -----
+//  GET
+// -----
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const { short_url } = req.params;
+
+  if (!short_url) return res.json({ error: "Invalid shortener" });
+
+  Shortener.findOne({ short_url: short_url })
+    .then((q) => {
+      if (!q) return res.json({ error: "Invalid shortener" });
+
+      return res.redirect(301, q.original_url);
+    })
+    .catch((e) => console.error(e));
 });
 
-app.listen(port, function() {
+// ------
+//  POST
+// ------
+app.post("/api/shorturl", function (req, res) {
+  const { url } = req.body;
+
+  // 1. Validar la URL
+  if (!isValidUrl(url)) {
+    return res.json({ error: "Invalid URL" });
+  }
+
+  // 2. Comprobar si existe, si existe lo retorno
+  Shortener.findOne({ original_url: url })
+    .then((q) => {
+      if (q) return res.json({ original_url: q.original_url, short_url: q.short_url });
+
+      // 3. Contar la cantidad de documentos de la colección para obtener el número de short
+      Shortener.countDocuments()
+        .then((counter) => {
+          // 4. Guardar nueva url
+          const short_url = counter + 1;
+
+          const newShortener = new Shortener({ original_url: url, short_url: short_url });
+
+          return newShortener.save();
+        })
+        .then((q) => {
+          return res.json({ original_url: q.original_url, short_url: q.short_url });
+        })
+        .catch((e) => {
+          console.error(e);
+          res.json({ error: "Error save database" });
+        });
+    })
+    .catch((e) => {
+      console.error(e);
+      res.json({ error: "Error find database" });
+    });
+});
+
+// --------
+//  LISTEN
+// --------
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
